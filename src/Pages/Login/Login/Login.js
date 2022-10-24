d793dabc3488747e74118c4da6fb8b14114db037
Mohammod Ali <mohammodali651@gmail.com>
2022-10-25 01:12:10 +0600

    login and register handle

--- src/Pages/Login/Login/Login.js
+++ src/Pages/Login/Login/Login.js
@@ -1,52 +1,72 @@
-import React, { useContext } from 'react';
-import { Button, Form } from 'react-bootstrap';
-import { useNavigate } from 'react-router-dom';
-import { AuthContext } from '../../../contexts/AuthProvider/AuthProvider';
-
+import React, { useContext, useState } from "react";
+import { Button, Form } from "react-bootstrap";
+import toast from "react-hot-toast";
+import { useLocation, useNavigate } from "react-router-dom";
+import { AuthContext } from "../../../contexts/AuthProvider/AuthProvider";
 
 const Login = () => {
-    const {signIn} = useContext(AuthContext)
-    const navigate = useNavigate()
+  const [error, setError] = useState("");
+  const { signIn, setLoading } = useContext(AuthContext);
+  const navigate = useNavigate();
+  const location = useLocation();
 
+  const from = location.state?.from?.pathname || "/";
 
-    const handleSubmit = event => {
-        event.preventDefault()
-        const form = event.target;
-        const email = form.email.value;
-        const password = form.password.value;
-        signIn(email, password)
-        .then( result => {
-            const user = result.user;
-            console.log(user)
-            form.reset()
-            navigate('/')
-        })
-        .catch( error => console.error(error))
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const email = form.email.value;
+    const password = form.password.value;
+    signIn(email, password)
+      .then((result) => {
+        const user = result.user;
+        console.log(user);
+        form.reset();
+        setError("");
 
-    }
+        if (user.emailVerified) {
+          navigate(from, { replace: true });
+        }else{
+          toast.error("Your email is not verified. Please verify your email")
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+        setError(error.message);
+      })
+      .finally(() => {
+        setLoading(false)
+      })
+  };
 
-    return (
-        <Form onSubmit={handleSubmit}>
+  return (
+    <Form onSubmit={handleSubmit}>
       <Form.Group className="mb-3" controlId="formBasicEmail">
         <Form.Label>Email address</Form.Label>
-        <Form.Control name='email' type="email" placeholder="Enter email" required />
-        
+        <Form.Control
+          name="email"
+          type="email"
+          placeholder="Enter email"
+          required
+        />
       </Form.Group>
 
       <Form.Group className="mb-3" controlId="formBasicPassword">
         <Form.Label>Password</Form.Label>
-        <Form.Control name='password' type="password" placeholder="Password" required />
+        <Form.Control
+          name="password"
+          type="password"
+          placeholder="Password"
+          required
+        />
       </Form.Group>
-     
-      
+
       <Button variant="primary" type="submit">
         Login
       </Button>
-      <Form.Text className="text-danger">
-          
-        </Form.Text>
+      <Form.Text className="text-danger">{error}</Form.Text>
     </Form>
-    );
+  );
 };
 
 export default Login;
--- src/contexts/AuthProvider/AuthProvider.js
+++ src/contexts/AuthProvider/AuthProvider.js
@@ -1,49 +1,83 @@
-import React, { createContext, useEffect, useState } from 'react';
-import {createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut} from 'firebase/auth'
-import app from '../../firebase/firebase.config';
+import React, { createContext, useEffect, useState } from "react";
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  onAuthStateChanged,
+  sendEmailVerification,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  signOut,
+  updateProfile,
+} from "firebase/auth";
+import app from "../../firebase/firebase.config";
+import { Spinner } from "react-bootstrap";
 
+export const AuthContext = createContext();
+const auth = getAuth(app);
 
-export const AuthContext = createContext()
-const auth = getAuth(app)
+const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
 
-const AuthProvider = ({children}) => {
-    const [user, setUser] = useState(null)
+  const providerLogin = (provider) => {
+    return signInWithPopup(auth, provider);
+  };
 
-    const providerLogin = (provider) => {
-        return signInWithPopup(auth, provider)
-    }
+  const createUser = (email, password) => {
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
 
-    const createUser = (email, password) => {
-        return createUserWithEmailAndPassword(auth, email, password)
-    }
+  const signIn = (email, password) => {
+    setLoading(true);
+    return signInWithEmailAndPassword(auth, email, password);
+  };
 
-    const signIn = (email, password) => {
-        return signInWithEmailAndPassword(auth, email, password)
-    }
+  const updateUserProfile = (profile) => {
+    return updateProfile(auth.currentUser, profile);
+  };
 
-    const logOut = () => {
-        return signOut(auth);
-    }
+  const verifyEmail = () => {
+    return sendEmailVerification(auth.currentUser);
+  };
 
-    useEffect( () =>{
-        const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
-            console.log('user inside state change')
-            setUser(currentUser)
-        })
+  const logOut = () => {
+    setLoading(true);
+    return signOut(auth);
+  };
 
-        return () => {
-            unsubscribe()
-        }
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      console.log("user inside state change");
+      if (currentUser == null || currentUser.emailVerified) {
+        setUser(currentUser);
+      }
+      setLoading(false);
+    });
 
-    },[])
+    return () => {
+      unsubscribe();
+    };
+  }, []);
 
-    const authInfo = { user, providerLogin, logOut, createUser, signIn }
+  const authInfo = {
+    user,
+    loading,
+    setLoading,
+    providerLogin,
+    logOut,
+    updateUserProfile,
+    verifyEmail,
+    createUser,
+    signIn,
+  };
 
-    return (
-        <AuthContext.Provider value={authInfo}>
-            {children}
-        </AuthContext.Provider>
-    );
+  if (loading) {
+    return <Spinner animation="border" variant="primary"></Spinner>;
+  }
+
+  return (
+    <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
+  );
 };
 
 export default AuthProvider;
